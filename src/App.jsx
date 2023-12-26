8b261f1403a96f7dd0e5cda5391348393d5f49e4
Abu Bakar <76771529+Abu0978123@users.noreply.github.com>
2023-12-26 21:57:29 +0500

    crud completed in firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import Auth from "./components/Auth";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, addDoc, deleteDoc, doc, updateDoc } from "firebase/firestore";
 import { db } from "./config/firebase";
 import './App.css'
 
@@ -12,25 +12,62 @@ function App() {
   const [newMovieTitle, setNewMovieTitle] = useState("");
   const [newReleaseDate, setNewReleaseDate] = useState(0);
   const [isNewMovieOscar, setIsNewMovieOscar] = useState(false);
-   console.log(newMovieTitle,newReleaseDate, isNewMovieOscar)
+  
+  // update the title 
+  const [update, setUpdate] = useState();
+  //  console.log(newMovieTitle,newReleaseDate, isNewMovieOscar)
   const movieCollectionRef = collection(db, "movies");
+  
+  // to render the movies list
+  const getData = async () => {
+    try {
+      const data = await getDocs(movieCollectionRef);
+      const filterData = data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      console.log(filterData);
+      setMovieList(filterData)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
-    const getData = async () => {
-      try {
-        const data = await getDocs(movieCollectionRef);
-        const filterData = data.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id,
-        }));
-        console.log(filterData);
-        setMovieList(filterData)
-      } catch (error) {
-        console.log(error);
-      }
-    };
     getData();
   }, []);
 
+
+  const onSubmitMovie = async (e) => {
+    e.preventDefault()
+    await addDoc(movieCollectionRef, 
+      {
+        title: newMovieTitle,
+        relDate: newReleaseDate,
+        gotOscar: isNewMovieOscar
+      }
+      ).then((res) => {getData() ,console.log(res, "data successfully added in firebase db")})
+      .catch((err) => {console.log(err.message, "some error in uploading new record")})
+  }
+
+  // delete the records
+  const deleteRecord = async(id) => {
+      const delRecord =  doc(db, "movies", id)
+      await deleteDoc(delRecord)
+      .then((res)=> { getData(),console.log("record deleted ", res)})
+      .catch((err) => {console.log("some error here", err)})
+  }
+
+  // update the record
+  const updated = async(id) => {
+    const delRecord =  doc(db, "movies", id)
+    await updateDoc(delRecord, {title: update})
+    .then((res)=> { getData(),console.log("record updated successfully ", res)})
+    .catch((err) => {console.log("some error here", err)})
+}
+
+
+ 
   return (
     <>
       <h1>Firebase Project 1</h1>
@@ -52,7 +89,7 @@ function App() {
         />
         <label> Received an Oscar</label>
         <button 
-        // onClick={onSubmitMovie}
+        onClick={onSubmitMovie}
         > Submit Movie</button>
       </div>
       <h2>Here is the Movies list</h2>
@@ -62,6 +99,9 @@ function App() {
               <h3 style={{color: items.gotOscar ? "green": "red"}}>{items.title}</h3>
                <h4>{items.relDate}</h4>
                <h4>{items.gotOscar}</h4>
+               <button onClick={()=> deleteRecord(items.id)}>Delete Record</button>
+               <input type="text" defaultValue={items.title} onChange={(e)=>setUpdate(e.target.value)}/>
+               <button onClick={()=> updated(items.id)}>updated</button>
            </div>
         ))
       }
